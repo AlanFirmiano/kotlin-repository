38accd4283aeaee277507604f61279f0cd008da1
alanfirmiano <alanfirmiano18@gmail.com>
2018-07-25 15:05:04 -0300

    usando for com dowTo

--- src/main/kotlin/com/example/demo/service/StudentService.kt
+++ src/main/kotlin/com/example/demo/service/StudentService.kt
@@ -12,12 +12,33 @@ class StudentService @Autowired constructor(private val studentRepository: Stude
 
     fun mostrar(): List<String> {
         var safe:Int? = null //safe call operator
-        val bomHumor = false
+        val bomHumor = true
         println("Hoje estou ${ if (bomHumor) "feliz" else "triste"} \nsafe : ${safe?: "sucesso"}") //elvis operator
+
+        val nota = 8
+        consultarNota(nota)
+        invertPrint(listaStudents)
 
         return listaStudents.sorted()
     }
 
+    private fun invertPrint(lista:List<Any>){
+        for(i in lista.size-1 downTo 0){
+            println("$i = ${lista[i]}")
+        }
+        println("...")
+    }
+    private fun consultarNota(nota:Int){
+        when(nota){
+            10, 9 -> println("Fantastico")
+            8, 7 -> println("Parabens")
+            6, 5, 4 -> println("Tem como recuperar")
+            in 0..3 -> println("Te vejo no proximo semestre")
+            else -> println("Nota invalida")
+        }
+    }
+
+    //CRUD
     fun save(student: Student):String {
         studentRepository.save(student)
         return "Sucess!"
@@ -36,6 +57,7 @@ class StudentService @Autowired constructor(private val studentRepository: Stude
         return "Sucess!"
     }
 
+    //MATH
     fun sum(num1: Double, num2: Double ): Double = num1 + num2
 
     fun sub(num1: Double, num2: Double ): Double = num1 - num2
